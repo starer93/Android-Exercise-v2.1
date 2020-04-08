c16c5bcb748b21d6bf29451d34750a9ae43da676
Yu <starer93@hotmail.com>
2020-04-08 11:24:09 +1000

    add fragment in activity

--- app/src/main/java/com/android/androidexercise/MainActivity.kt
+++ app/src/main/java/com/android/androidexercise/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.android.androidexercise
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.android.androidexercise.base.BaseActivity
+import com.android.androidexercise.base.BasePresenter
+import com.android.androidexercise.ui.news.NewsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val newsFragment = NewsFragment.getInstance()
+    override fun getLayoutResId(): Int = R.layout.activity_main
+
+    override fun getPresenter(): BasePresenter<*>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFragment()
+    }
+
+    private fun initFragment() {
+        val transition = supportFragmentManager.beginTransaction()
+        transition.replace(R.id.layout_main, newsFragment)
+        transition.commit()
+    }
+
+    fun updateTitle(title: String) {
+        supportActionBar?.title = title
     }
 }
